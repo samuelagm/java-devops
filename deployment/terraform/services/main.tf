e2ea3b4db0d6866aa3249041a676fcdcbde69baf
Samuel Agbonkpolo <samuelagm@gmail.com>
2024-01-16 04:21:15 +0100

    feat: exposed application properties as vars and secrets

--- deployment/terraform/services/main.tf
+++ deployment/terraform/services/main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
-    digitalocean = { 
-      source = "digitalocean/digitalocean"
-      version = "~> 2.0" 
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
   }
 }
 
-provider helm {
-    kubernetes {
-        host  = data.digitalocean_kubernetes_cluster.ecabs_cluster.endpoint
-        token = data.digitalocean_kubernetes_cluster.ecabs_cluster.kube_config[0].token
-        cluster_ca_certificate = base64decode(
-            data.digitalocean_kubernetes_cluster.ecabs_cluster.kube_config[0].cluster_ca_certificate
-        )
-    }
+provider "helm" {
+  kubernetes {
+    host  = data.digitalocean_kubernetes_cluster.ecabs_cluster.endpoint
+    token = data.digitalocean_kubernetes_cluster.ecabs_cluster.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      data.digitalocean_kubernetes_cluster.ecabs_cluster.kube_config[0].cluster_ca_certificate
+    )
+  }
 }
 
 provider "digitalocean" {
@@ -31,4 +31,14 @@ resource "helm_release" "rabbitmq" {
   chart            = "rabbitmq"
   create_namespace = true
   namespace        = "broker"
+
+  set {
+    name  = "auth.username"
+    value = var.rabbitmq_username
+  }
+
+  set {
+    name  = "auth.password"
+    value = var.rabbitmq_password
+  }
 }
--- deployment/terraform/services/vars.tf
+++ deployment/terraform/services/vars.tf
@@ -4,3 +4,15 @@ variable "cluster_name" {
   type    = string
   default = "main"
 }
+
+variable "rabbitmq_username" {
+  type    = string
+  default = "guest"
+}
+
+variable "rabbitmq_password" {
+  type    = string
+  default = "guest"
+  sensitive = true
+}
+
